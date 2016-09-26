9ed1478b9883d96efdf199adae779fe5b8039ab6
Manuel Zafra <m.zafra@camposelectricas.es>
2016-09-26 13:51:12 +0200

    Primer acercamiento a servidor

--- client/templates/surveys/enc_ini.js
+++ client/templates/surveys/enc_ini.js
@@ -1,10 +1,29 @@
+/*
+AutoForm.addHooks('enc-ini', {
+  // Called when any submit operation succeeds
+  onSuccess: function(formType, result) {
+    console.log("ok");
+  },
+  // Called when any submit operation fails
+  onError: function(formType, error) {
+    console.log("no ok");
+  }
+});
+*/
 Template.encIni.events({
-  "click .btn-danger": function(event, template){
-    Meteor.call("queryDelete", this._id, function(error, result){
-      if(error){
-        throwError(error);
-      }
-      Router.go('/');
-    });
+  'submit form':function(e){
+    e.preventDefault();
+
+    var answer = {
+      q1 : $(e.target).find('[name=age]').val(),
+      q2 : $(e.target).find('[name=select1]').val(),
+      q3 : $(e.target).find('[name=readEmoticons]').val(),
+      q4 : $(e.target).find('[name=entender]').val(),
+      q5 : $(e.target).find('[name=writeEmoticons]').val(),
+      q6 : $(e.target).find('[name=razonesNo]').val(),
+      q7 : $(e.target).find('[name=automated]').val()
+    }
+
+    Meteor.call("submitEncIni", answer);
   }
 });
---
+++ server/methods/survey.js
@@ -0,0 +1,6 @@
+Meteor.methods({
+  submitEncIni:function(answer){
+    console.log(answer);
+    console.log("Hey from server: "+answer.q2);
+  }
+});
